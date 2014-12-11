89f9866bd3eb8ebfcdfb3011abcef53b4620403c
Kai Jauslin <kai.jauslin@cando-image.com>
2014-12-11 10:39:11 +0100

    Added deploy script for installation

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+scp scripts/WhiteRabbit*.py pi@10.0.0.1:/home/pi/schneehasen/scripts/
+scp scripts/conf.template.py pi@10.0.0.1:/home/pi/schneehasen/scripts/
+scp -r scripts/actions/*.py pi@10.0.0.1:/home/pi/schneehasen/scripts/
+scp scripts/*.csv pi@10.0.0.1:/home/pi/schneehasen/scripts/
